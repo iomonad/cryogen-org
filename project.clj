bcef4bacdfbd911ec75495dfa0233930cd51727d
iomonad <iomonad@riseup.net>
2022-02-26 22:31:25 +0100

    initial commit

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject cryogen-org "0.1.0"
+  :description "A Org-mode module for the Cryogen engine"
+  :url "https://github.com/iomonad/cryogen-org"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [cryogen-core "0.3.2"]]
+  :repl-options {:init-ns user})
---
+++ src/cryogen_org/core.clj
@@ -0,0 +1,6 @@
+(ns cryogen-org.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
