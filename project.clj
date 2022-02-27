0504417548428948734f7dd766728d3fcfd9cdac
iomonad <iomonad@riseup.net>
2022-02-27 11:33:13 +0100

    release: 0.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cryogen-org "0.1.0"
+(defproject cryogen-org "0.1.1"
   :description "A Org-mode module for the Cryogen engine"
   :url "https://github.com/iomonad/cryogen-org"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
--- test/cryogen_org/core_test.clj
+++ test/cryogen_org/core_test.clj
@@ -1,3 +1,11 @@
 (ns cryogen-org.core-test
   (:require [clojure.test :refer :all]
-            [cryogen-org.core :refer :all]))
+            [cryogen-org.core :refer :all])
+  (:import [cryogen_core.markup Markup]))
+
+;;; Fixtures
+
+(use-fixtures :once
+  (fn [body]
+    (let [test-ctx true]
+      body)))
