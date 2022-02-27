c5e742aa98871c77a22433e36b3c8b48a8bf1b8f
iomonad <iomonad@riseup.net>
2022-02-27 11:41:25 +0100

    release: 0.1.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cryogen-org "0.1.1"
+(defproject io.trosa/cryogen-org "0.1.2"
   :description "A Org-mode module for the Cryogen engine"
   :url "https://github.com/iomonad/cryogen-org"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
